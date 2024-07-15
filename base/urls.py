b6c98656285f484070c49d40b4c7d4651e0f93b9
shrey sharma <shrey7shrey@gmail.com>
2024-07-16 00:50:14 +0530

    hey uploading my project studybuddy a dedicated chat and learn platform for students

--- base/urls.py
+++ base/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     path('delete-message/<str:pk>/', views.deleteMeesage, name='delete-message'),
-path('update-user/', views.updateUser, name='update-user'),
+    path('update-user/', views.updateUser, name='update-user'),
 ]
