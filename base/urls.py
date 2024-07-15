24cc7fb56f7c427da1d730ba86f3191877a57f42
shrey sharma <shrey7shrey@gmail.com>
2024-07-16 02:15:51 +0530

    hey uploading my project studybuddy a dedicated chat and learn platform for students

--- base/urls.py
+++ base/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     #dynamic url routing, primary key string
     path('room/<str:pk>/', views.room, name='room'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
-
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
